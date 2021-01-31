2690928ff126eccd3dbfa7551cb39cf93c666ba4
KornelLigner <kornelligner@wp.pl>
2021-01-31 20:08:59 +0100

    Dodaj pliki projektów.

---
+++ WebApplicationKornel/WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplicationKornel.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p>
+            <asp:Label ID="Label1" runat="server" Text="Wpisz swoje imie i nazwisko"></asp:Label>
+        </p>
+        <p>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click1" Text="OK" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebApplicationKornel/WebForm1.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace WebApplicationKornel
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack == true)
+            {
+                Label1.Text = ("Super, dodano! :)");
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection stormconn = new SqlConnection("Server=tcp:korneldatabase.database.windows.net,1433;Initial Catalog=kornelDataBase;Persist Security Info=False;User ID=kornelligner;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            {
+                SqlCommand insert = new SqlCommand("Exec dbo.InsertFullname @Fullname", stormconn);
+                insert.Parameters.AddWithValue("@Fullname", TextBox1.Text);
+
+                stormconn.Open();
+                insert.ExecuteNonQuery();
+                stormconn.Close();
+
+                if (IsPostBack)
+                {
+                    TextBox1.Text = "";
+                }
+            }
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
